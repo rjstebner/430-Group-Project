6437055a21966d55ea94f6fb18e88fb7f76432b3
eworsham <eworsham64@gmail.com>
2025-01-27 16:35:43 -0500

    Just getting started

---
+++ my-app/src/app/ui/catalog-card.tsx
@@ -0,0 +1,9 @@
+export default function CatalogCard() {
+    return (
+        <div>
+            <h2>Card Title</h2>
+            <p>Card Description</p>
+            <p>ect</p>
+        </div>
+    )
+}
