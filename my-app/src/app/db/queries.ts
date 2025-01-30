9b41c18b12886406eec793af1dbe110e3cc90f35
Heitor Cazado <heitorcazado@gmail.com>
2025-01-29 22:00:30 -0300

    Auth working with signin with google and base layout

--- my-app/src/app/db/queries.ts
+++ my-app/src/app/db/queries.ts
@@ -1,3 +1,52 @@
 import pool from './connection';
 
 //the place to put actions for accessing the db
+export async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await pool<User>`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
+
+export async function insertUser(
+  name: string,
+  email: string,
+  password: string,
+  type: string,
+  registration_dt: string
+) {
+  try {
+    const res = await pool`INSERT INTO users (username, email, password, type)
+                VALUES(${name}, ${email}, ${password}, ${type} )`;
+    console.log(`DB: user inserted succesfuly ${res.rows[0]}`);
+  } catch (err) {
+    console.error('Database failed to insert new user:', err);
+  }
+}
+
+export async function insertSocialUser(
+  name: string,
+  email: string,
+  type: string,
+  registration_dt: string
+) {
+  try {
+    const res = await pool`INSERT INTO users (username, email, type)
+                VALUES(${name}, ${email} ${type} )`;
+    console.log(`DB: user inserted succesfuly ${res.rows[0]}`);
+  } catch (err) {
+    console.error('Database failed to insert new user:', err);
+  }
+}
+
+export const testConnection = async () => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    console.log('Database connected! Current time:', result.rows[0].now);
+  } catch (err) {
+    console.error('Database connection failed:', err);
+  }
+};
