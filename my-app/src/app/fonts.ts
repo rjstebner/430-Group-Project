547434daa6306bec0b44e427bd23dcde48c7df13
eworsham <eworsham64@gmail.com>
2025-02-03 17:49:09 -0500

    Updated fonts

---
+++ my-app/src/app/fonts.ts
@@ -0,0 +1,5 @@
+import { Roboto } from "next/font/google";
+import { Playwrite_VN } from "next/font/google";
+
+export const roboto = Roboto({ weight: ["100", "300", "400", "500", "700", "900"] });
+export const playwrite_vn = Playwrite_VN();
