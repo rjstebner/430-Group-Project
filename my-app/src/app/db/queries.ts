e91294f85b73638f9c16c1b3b85bb847139c3217
rjstebner <rjstebner@gmail.com>
2025-02-06 11:35:31 +0900

    fixed syntax

--- my-app/src/app/db/queries.ts
+++ my-app/src/app/db/queries.ts
@@ -57,6 +57,7 @@ export const deleteProduct = async (id: number): Promise<boolean> => {
     } catch (error) {
         console.error('Error deleting product:', error);
         return false;
+    };
 //the place to put actions for accessing the db
 
 export async function fetchProducts() {
