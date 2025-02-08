2209e1cff5312caa70f57445e9057d9d2e8fe81d
Andrew Halisky <ajhalisky@gmail.com>
2025-02-08 16:22:23 -0500

    Completed nav-bar

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,12 +3,9 @@ import CatalogCards from "./ui/catalog-cards";
 import NavBar from "./ui/nav-bar";
 
 export default function Home() {
-  let items = ["Home","Items","Account"];
   return (
     <div>
-      <NavBar 
-      navItems={items}
-      />
+      <NavBar />
       <h1 className={`text-3xl m-5 ${playwrite_vn.className}`}>Catalog</h1>
       <CatalogCards />
     </div>
--- my-app/src/app/ui/nav-bar.tsx
+++ my-app/src/app/ui/nav-bar.tsx
@@ -1,69 +1,25 @@
 "use client";
-import { useState } from "react";
+// import { useState } from "react";
+import * as React from "react";
 
-interface NavBarProps {
-    navItems: string[];
-}
+// interface NavBarProps {
+// }
 
-export default function NavBar({ navItems }: NavBarProps) {
-    const [selectedIndex, setSelectedIndex] = useState(-1);
+export default function NavBar() {
     return (
-    <nav className="navbar navbar-expand-md navbar-light bg-white shadow">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-                <img 
-                width="50"
-                height="50"
-                className="d-inline-block align-center"
-                alt=""
-                />
-            <span className="navbar-toggler-icon" />
-            </a>
-                <button className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarContent"
-                    aria-controls="navbarContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-        </button>
-                <div
-                    className="collapse navbar-collapse"
-                    id="navbarContent"
-                >
-                    <ul className="navbar-nav me-auto mb-2 mb-md-1">
-                        {navItems.map((items, index) => (
-                            <li
-                                key={items}
-                                className="nav-item"
-                                onClick={() => setSelectedIndex(index)}
-                            >
-                                <a
-                                    className={
-                                        selectedIndex == index
-                                            ? "nav-link active fw-bold"
-                                            : "nav-link"}
-                                    href="#"
-                                >
-                                    {items}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                    <form className="d-flex me-3">
-                        <input
-                            className="form-control me-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                        />
-                        <button className="btn btn-outline" type="submit">
-                            Search
-                        </button>
-                    </form>
-                </div>
+      <div>
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-brand">
+              <a className="navbar-brand" href='/'></a>
             </div>
+            <ul className="nav navbar-nav">
+              <li><a href='/'>Home</a></li>
+              <li><a href='/catalog'>Catelog</a></li>
+              <li><a href='/account'>Account</a></li>
+            </ul>
+          </div>
         </nav>
-    );
+      </div>
+    )
 }
