f96a18e5fdabe6e3357422290d5e7dbf0bf820db
eworsham <eworsham64@gmail.com>
2025-02-11 16:19:08 -0500

    Added font subset

--- my-app/src/app/fonts.ts
+++ my-app/src/app/fonts.ts
@@ -1,5 +1,5 @@
 import { Roboto } from "next/font/google";
 import { Playwrite_VN } from "next/font/google";
 
-export const roboto = Roboto({ weight: ["100", "300", "400", "500", "700", "900"] });
+export const roboto = Roboto({ weight: ["100", "300", "400", "500", "700", "900"], subsets: ['latin'] });
 export const playwrite_vn = Playwrite_VN();
