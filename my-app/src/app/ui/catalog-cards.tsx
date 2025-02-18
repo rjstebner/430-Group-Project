131dfe1f8acd8ea2112e35dcdd808c51893f1983
eworsham <eworsham64@gmail.com>
2025-02-18 18:39:12 -0500

    implemented product search

--- my-app/src/app/ui/catalog-cards.tsx
+++ my-app/src/app/ui/catalog-cards.tsx
@@ -1,26 +1,40 @@
+"use client"
+import { useState } from "react";
 import Image from "next/image";
-import { fetchProducts } from "../db/queries"
-// import fetchReviews from "ratings-reviews";
+import { Product } from "../db/types";
 
-export default async function CatalogCards() {
-    const products = await fetchProducts();
+export default function CatalogCards({ products } : { products: Product[] }) {
+    const [query, setQuery] = useState("");
+
+    const filteredProducts = products.filter((product) => {
+        return product.name.toLowerCase().includes(query.toLowerCase())
+    })
 
     return (
-        <div className="md:grid grid-cols-3">
-            {products.map(product => (
-                <div key={product.id} className="border border-solid bg-gray-100 p-5 m-4 rounded-xl">
-                    <p className="m-3 font-bold">{product.name}</p>
-                    <p className="m-3">${product.price}</p>
-                    <Image 
-                        src={product.image || '/images/placeholder.jpg'}
-                        alt={`${product.name} image`}
-                        width={200}
-                        height={200}
-                        className="m-auto border border-solid border-gray-300"
-                    />
-                    <a href="" className="block p-3 m-5 bg-blue-300 w-xs rounded-xl text-center">Product Details</a>
-                </div>
-            ))}
-        </div>
+        <>
+            <input 
+                type="text" 
+                placeholder="Search products..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                className="border border-gray-400 p-2 m-4 rounded-xl"
+            />
+            <div className="md:grid grid-cols-3">
+                {filteredProducts.map(product => (
+                    <div key={product.id} className="border border-solid bg-gray-100 p-5 m-4 rounded-xl">
+                        <p className="m-3 font-bold">{product.name}</p>
+                        <p className="m-3">${product.price}</p>
+                        <Image 
+                            src={product.image || '/images/placeholder.jpg'}
+                            alt={`${product.name} image`}
+                            width={200}
+                            height={200}
+                            className="m-auto border border-solid border-gray-300"
+                        />
+                        <a href="" className="block p-3 m-5 bg-blue-300 w-xs rounded-xl text-center">Product Details</a>
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
