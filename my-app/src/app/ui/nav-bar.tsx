c5e516d7b893366c1d4e273cb3c3adf765f98000
eworsham <eworsham64@gmail.com>
2025-02-19 22:00:11 -0500

    Added admin to nav bar

--- my-app/src/app/ui/nav-bar.tsx
+++ my-app/src/app/ui/nav-bar.tsx
@@ -14,10 +14,11 @@ export default function NavBar() {
             <div className="navbar-brand">
               <Link className="navbar-brand" href='/'></Link>
             </div>
-            <ul className="nav navbar-nav">
+            <ul className="nav navbar-nav w-100">
               <li><Link href='/'>Catalog</Link></li>
-              <li><Link href='/account'>Account</Link></li>
+              <li><Link href='/admin'>Admin</Link></li>
               <li><Link href='/contact'>Contact Us</Link></li>
+              <li><Link href='/login'>Login</Link></li>
             </ul>
           </div>
         </nav>
