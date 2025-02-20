d42c1b750bd6adff51517c4d9a423df4d75e19f9
Andrew Halisky <ajhalisky@gmail.com>
2025-02-19 21:17:28 -0500

    Adjusted colors in contact page

--- my-app/src/app/contact/page.tsx
+++ my-app/src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Contact() {
     return (
-        <div className="bg-slate-300 p-4 mt-6 mb-12">
+        <div className="bg-gray-100 p-4 mt-6 mb-12">
             <h1 className="text-3xl">Contact Us!</h1>
             <form>
                 <label htmlFor="fname">First Name</label>
@@ -15,7 +15,7 @@ export default function Contact() {
                 <input type="tel" id="phone" name="phone" required />
                 <label htmlFor="message">Message</label>
                 <textarea id="message" name="message" defaultValue="Type your message here" required />
-                <input className="bg-blue-300 p-3" type="submit" defaultValue="Submit" required />
+                <input className="bg-ultraViolet text-white p-3 rounded-xl text-center" type="submit" defaultValue="Submit" required />
             </form>
         </div>
     );
